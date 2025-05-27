1985688219582d5d1577d96844f5c987c37140c4
unknown <vishurudra@gmail.com>
2025-05-27 16:18:03 +0530

    redux'

--- muieg/src/Nav.js
+++ muieg/src/Nav.js
@@ -1,15 +1,20 @@
 import { Route, Routes, Link } from "react-router-dom";
 import Counter from "./Counter";
 import Todos from "./Todos";
+import TodosById from "./TodosById";
 
 export default function Nav() {
   return (
     <div>
-      <Link to={"/"}>Home</Link><br/>
+      <Link to={"/"}>Home</Link>
+      <br />
       <Link to={"todos"}>Todos</Link>
+      <br />
+      <Link to={"todosbyid"}>TodosById</Link>
       <Routes>
         <Route path="/" element={<Counter />} />
         <Route path="todos" element={<Todos />} />
+        <Route path="todosbyid" element={<TodosById />} />
       </Routes>
     </div>
   );
---
+++ muieg/src/TodosById.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Table from "./Table";
+import Button from "@mui/material/Button";
+
+export default function TodosById() {
+  const [todoId, setTodoId] = useState(0);
+  const [todo, setTodo] = useState([]);
+
+  const onChange = (e) => setTodoId(e.target.value);
+  const onFetch = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos/"+todoId)
+      .then((data) => data.json())
+      .then((data) => setTodo([data]));
+  };
+  return (
+    <div>
+      <TextField
+        id="outlined-basic"
+        label="Enter Todo Id"
+        variant="outlined"
+        onChange={onChange}
+      />
+      <br />
+      <Button variant="contained" onClick={onFetch}>
+        Fetch Todo by Id
+      </Button>
+      <Table rows={todo} />
+    </div>
+  );
+}
