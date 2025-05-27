f56e6a68a37813c36058b1d9e93791c38940cbfb
unknown <vishurudra@gmail.com>
2025-05-27 16:02:04 +0530

    redux'

--- muieg/src/Todos.js
+++ muieg/src/Todos.js
@@ -1,19 +1,29 @@
 import Button from "@mui/material/Button";
 import { useState } from "react";
 import Table from "./Table";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Todos() {
   const [todos, setTodos] = useState([]);
-  const fetchTodos = () =>
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchTodos = () => setIsLoading(true);
+  setTimeout(() => {
     fetch("https://jsonplaceholder.typicode.com/todos/")
       .then((data) => data.json())
-      .then((data) => setTodos(data));
+      .then((data) => {
+        setTodos(data);
+        setIsLoading(false);
+      });
+  }, 10000);
+
   return (
     <div>
       <Button variant="contained" onClick={fetchTodos}>
         Fetch Todos
       </Button>
-      <Table rows={todos}/>
+      <br />
+      {isLoading && <CircularProgress hidden={true} />}
+      <Table rows={todos} />
     </div>
   );
 }
