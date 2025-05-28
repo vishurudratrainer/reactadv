076631acc414d232e88cd52c27bd53d978781a9b
unknown <vishurudra@gmail.com>
2025-05-28 10:50:11 +0530

    done

--- eb/src/Fallback.js
+++ eb/src/Fallback.js
@@ -3,6 +3,7 @@ const Fallback=({error,resetErrorBoundary})=>{
     return(<div>
         <p>Something went wrong</p>
         <pre style={{color:"red"}}>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try Again</button>
     </div>)
 
 }
