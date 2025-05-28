f0fea01943a4575e4055f62496ba5ddde452d194
unknown <vishurudra@gmail.com>
2025-05-28 11:19:35 +0530

    done

--- eb/src/App.js
+++ eb/src/App.js
@@ -3,18 +3,18 @@ import { ErrorBoundary } from "react-error-boundary";
 import Todos from "./Todos";
 import Fallback from "./Fallback";
 import Todos1 from "./Todos1";
-const logError =(error,componentStack)=>{
-  console.error("Error",error)
-  console.log("compE",componentStack)
-}
+const logError = (error, componentStack) => {
+  console.error("Error", error);
+  console.log("compE", componentStack);
+};
 function App() {
   return (
-     <ErrorBoundary  FallbackComponent={Fallback} onError={logError} >
-      <div className="App">
+    <div className="App">
+      <ErrorBoundary FallbackComponent={Fallback} onError={logError}>
         <Todos />
-        <Todos1/>
-      </div>
-    </ErrorBoundary>
+      </ErrorBoundary>{" "}
+      <Todos1 />
+    </div>
   );
 }
 
