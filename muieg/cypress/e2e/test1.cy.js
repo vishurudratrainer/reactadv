24f4c7c63d44f9d023a54cd0d33374d358870c21
unknown <vishurudra@gmail.com>
2025-05-28 15:26:44 +0530

    done

--- muieg/cypress/e2e/test1.cy.js
+++ muieg/cypress/e2e/test1.cy.js
@@ -11,7 +11,7 @@ describe("template spec", () => {
     cy.visit("http://localhost:3000/todosbyid");
   });
 
-  it.only("Todos todosbyid select to do by Id", () => {
+  it("Todos todosbyid select to do by Id", () => {
     cy.visit("http://localhost:3000/todosbyid");
     cy.get("#todoId").type("23");
     cy.get("#todoId")
@@ -21,10 +21,26 @@ describe("template spec", () => {
     cy.get("#fetchTodoById").click();
     cy.get("[data-field='userId']").should("have.length", 1);
     cy.get("[data-field='userId']").should("have.text", "USERID2");
+    cy.get("#todoId").screenshot();
   });
 
   it("Todos todosbyid done test", () => {
     cy.visit("http://localhost:3000/todosbyid");
     cy.xpath("/html/body/div/div/div/div/div[1]/div/input").type("66");
+  });
+  it("Todos intercept done test", () => {
+    cy.intercept("GET", "https://jsonplaceholder.typicode.com/todos/132", {
+      fixture: "todos.json",
+    })
+    cy.visit("http://localhost:3000/todosbyid");
+    cy.get("#todoId").type("132");
+    cy.get("#todoId")
+      .invoke("val")
+      .then((val) => console.log("Value", val));
+
+    cy.get("#fetchTodoById").click();
+    cy.get("[data-field='userId']").should("have.length", 1);
+    cy.get("[data-field='userId']").screenshot();
+    cy.get("[data-field='userId']").should("have.text", "USERID123");
   });
 });
