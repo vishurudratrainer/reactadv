4dd1c6366f3e47493f8e9d45adaf16a37741ddbb
unknown <vishurudra@gmail.com>
2025-05-28 15:01:34 +0530

    done

--- muieg/cypress/e2e/test1.cy.js
+++ muieg/cypress/e2e/test1.cy.js
@@ -11,13 +11,20 @@ describe("template spec", () => {
     cy.visit("http://localhost:3000/todosbyid");
   });
 
-    it("Todos todosbyid select to do by Id", () => {
+  it.only("Todos todosbyid select to do by Id", () => {
     cy.visit("http://localhost:3000/todosbyid");
-    cy.get("#todoId").type("23")
-    cy.get("#todoId").invoke('val').then(val => console.log("Value",val))
+    cy.get("#todoId").type("23");
+    cy.get("#todoId")
+      .invoke("val")
+      .then((val) => console.log("Value", val));
 
-    cy.get("#fetchTodoById").click()
-    cy.get("[data-field='userId']").should("have.length",1)
-    cy.get("[data-field='userId']").should("have.text","USERID2")
+    cy.get("#fetchTodoById").click();
+    cy.get("[data-field='userId']").should("have.length", 1);
+    cy.get("[data-field='userId']").should("have.text", "USERID2");
+  });
+
+  it("Todos todosbyid done test", () => {
+    cy.visit("http://localhost:3000/todosbyid");
+    cy.xpath("/html/body/div/div/div/div/div[1]/div/input").type("66");
   });
 });
