bce2b0b71e9a88dc7898732323af5695177cc5c3
unknown <vishurudra@gmail.com>
2025-05-28 16:32:57 +0530

    done

--- portaleg/src/Modal.js
+++ portaleg/src/Modal.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
+import './App.css';
 
 export default function Modal({children}){
     const modalRoot = document.getElementById("modal")
-    return ReactDOM.createPortal((<div>{children}</div>),modalRoot)
+    return ReactDOM.createPortal((<div className='w3-modal'>{children}</div>),modalRoot)
 }
