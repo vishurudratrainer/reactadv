64761a15eeb456977393a0eef2b1017294d9024e
unknown <vishurudra@gmail.com>
2025-06-09 11:11:55 +0530

    commit 5

--- jpmcday1/src/rest/RestInstance.js
+++ jpmcday1/src/rest/RestInstance.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const RestInstance = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com/",
   timeout: 5000,
@@ -7,6 +6,30 @@ const RestInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+RestInstance.interceptors.request.use(
+  (request) => {
+    // request.headers['Authorization'] = 'Bearer token'; // Add authorization header
+    console.log("Inside request interceptor");
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+RestInstance.interceptors.response.use(
+  (response) => {
+    // request.headers['Authorization'] = 'Bearer token'; // Add authorization header
+    console.log("Inside response interceptor");
+    let stringified = JSON.stringify(response.data);
+    console.log(stringified)
+    response.data = (stringified.toUpperCase());
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 
-export default RestInstance
+export default RestInstance;
