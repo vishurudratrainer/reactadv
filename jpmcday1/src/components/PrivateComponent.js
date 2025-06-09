afcf9983c3be929eeece2e57d1bbbdf9ea698b20
unknown <vishurudra@gmail.com>
2025-06-09 14:56:47 +0530

    commit 5

--- jpmcday1/src/components/PrivateComponent.js
+++ jpmcday1/src/components/PrivateComponent.js
@@ -1,11 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateComponent({ authenticated }) {
   if (authenticated) return  <Outlet />;
   else
     return (
-      <div>
-        To access page please login<Link to={"/"}>To Login click here</Link>
-      </div>
+    <Navigate to={"/"}></Navigate>
     );
 }
