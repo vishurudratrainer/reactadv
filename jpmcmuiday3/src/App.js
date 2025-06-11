4be37be061abd43da4ede15998c07c9f76470624
unknown <vishurudra@gmail.com>
2025-06-11 11:44:35 +0530

    grid

---
+++ jpmcmuiday3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Todos from './Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Todos/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ jpmcmuiday3/src/Todos.js
@@ -0,0 +1,16 @@
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+
+export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  const fetchTodos = () => {
+    fetch("https://jsonplaceholder.typicode.com/todos").then(data=>data.json()).then(data=>setTodos(data))
+  };
+  return (
+    <div>
+      <Button variant="contained" onClick={fetchTodos}>Fetch Todos</Button>
+      <Alert severity="success">{JSON.stringify(todos)} </Alert>
+    </div>
+  );
+}
