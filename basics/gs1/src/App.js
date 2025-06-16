422cf6c5eb15f7de2582898da5017cdb7a8312f4
unknown <vishurudra@gmail.com>
2025-06-16 16:01:34 +0530

    basics

--- basics/gs1/src/App.js
+++ basics/gs1/src/App.js
@@ -3,14 +3,18 @@ import Calculator from "./Calculator";
 import Counter from "./Counter";
 import SelectColor from "./SelectColor";
 import StudentForm from "./StudentForm";
-
+import { useState } from "react";
 function App() {
+  const [showCalc, setShowCalc] = useState(true);
+  const changeShowCalc = () => setShowCalc(!showCalc);
   return (
     <div className="App">
       <header className="App-header">
-        <SelectColor/>
-        <StudentForm/>
-        <Calculator/>
+        <input type="checkbox" checked={showCalc} onClick={changeShowCalc} />
+        Cal Visible
+        <SelectColor />
+        <StudentForm />
+        {showCalc && <Calculator />}
         <Counter />
       </header>
     </div>
