c7a11407dbd908e94483e2b7ef9d55a34b040587
unknown <vishurudra@gmail.com>
2025-06-16 12:30:21 +0530

    basics

--- basics/gs1/src/Counter.js
+++ basics/gs1/src/Counter.js
@@ -1,14 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter(counter + 1);
+    setAction("INCREMENT");
+  };
 
-const Counter = ()=>{
-    const [counter,setCounter]=useState(0)
-    const increment=()=>setCounter(counter+1)
+  const decrement = () => {
+    setCounter(counter - 1);
+    setAction("DECREMENT");
+  };
+  const [action, setAction] = useState("");
 
-    return(<div>
-        <h1>Counter {counter}</h1>
-        <button onClick={increment}>INCREMENT</button>
-    </div>)
-}
+  return (
+    <div>
+      <h1>Counter {counter} Action :{action}</h1>
+      <button onClick={increment}>INCREMENT</button>
+      <button onClick={decrement}>DECREMENT</button>
+    </div>
+  );
+};
 
 export default Counter;
