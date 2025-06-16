2de0f621296a56e2f6913d2a7570dbb88efbf464
unknown <vishurudra@gmail.com>
2025-06-16 15:49:54 +0530

    basics

--- basics/gs1/src/App.js
+++ basics/gs1/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Calculator from "./Calculator";
 import Counter from "./Counter";
+import SelectColor from "./SelectColor";
 import StudentForm from "./StudentForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SelectColor/>
         <StudentForm/>
         <Calculator/>
         <Counter />
---
+++ basics/gs1/src/SelectColor.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+export default function SelectColor() {
+  const [color, setColor] = useState("");
+  const [colors,] = useState(["red", "green", "blue"]);
+  const changeColor = (e) => setColor(e.target.value);
+
+  return (
+    <div>
+      <select onChange={changeColor}>
+        <option>Please select color</option>
+        {colors.map((color) => (
+          <option value={color}>{color}</option>
+        ))}
+      </select>
+      <textarea style={{ backgroundColor: color }}>Color will change</textarea>
+    </div>
+  );
+}
